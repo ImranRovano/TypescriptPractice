c3c0653370a6b3315e0df8f11a5700d3589b985c
ImranHuhn <imranhuhn1@gmail.com>
2023-02-04 11:56:57 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+"use strict";
+// export{}
+// const username = "imran";
+// let game: string;
+// game = "Basketball";
+// game = "Tennis";
+exports.__esModule = true;
+var print = function (name) { return console.log(name); };
+var double = function (number) { return number * 2; };
+print("Joe");
+console.log(double(7));
---
+++ app.ts
@@ -0,0 +1,43 @@
+// export{}
+// const username = "imran";
+// let game: string;
+// game = "Basketball";
+// game = "Tennis";
+
+// function print(game: string, points: number){
+//     console.log(`Billy got ${points} points in his last ${game} game`)
+// }
+// print(game, 23);
+
+
+
+// let a: number = 1;
+// let b: string = "string";
+// let c: boolean = true;
+// let d: null = null;
+// let e: object = {};
+// let f: undefined = undefined;
+// let g: void = null; //void can either be undefined or null
+// let h: any = "j";
+// let i: any[] = ["billy", 23]; //any type of array
+// let j: Array<string> = ["23"];
+// let k: Array<number> = [23, 44];
+
+//////////////////////////////////////////////
+
+//export{};
+// function print(name){
+//     console.log(name);
+// }
+
+//export{};
+// let print = (name, age, location) => {
+//     console.log(name); 
+// }
+// print("Joe", 28, "CT")
+
+// export{};
+// let print = name => console.log(name); 
+// let double = number => number * 2;
+// print("Joe");
+// console.log(double(7));
